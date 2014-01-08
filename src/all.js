3810b5a6801eb3da2f29288ea07ad7ec9365dc95
kidomah <kidomah@gmail.com>
2014-01-08 13:58:37 +0900

    Fix a bug

--- src/all.js
+++ src/all.js
@@ -113,7 +113,7 @@ var __extends = this.__extends || function (d, b) {
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
-}
+};
 var S = (function () {
     function S(exp) {
         this.exp = exp;
@@ -248,7 +248,7 @@ var SLambda = (function (_super) {
         return function (args) {
             newenv.update(variables, args);
             return expression.evaluate(newenv);
-        }
+        };
     };
     return SLambda;
 })(S);
@@ -283,149 +283,133 @@ var SProc = (function (_super) {
 function ast(exps) {
     if(typeof exps === 'string') {
         return new SSymbol(exps);
-    } else {
-        if(!(exps instanceof Array)) {
-            return new SNum(exps);
-        } else {
-            if(exps[0] === 'quote') {
-                return new SQuote([
-                    new SStr(exps[0]), 
-                    exps[1], 
-                    
-                ]);
-            } else {
-                if(exps[0] === 'if') {
-                    return new SIf([
-                        new SStr(exps[0]), 
-                        ast(exps[1]), 
-                        ast(exps[2]), 
-                        ast(exps[3]), 
-                        
-                    ]);
-                } else {
-                    if(exps[0] === 'define') {
-                        if(exps[1] instanceof Array) {
-                            var variables = [];
-                            var expressions = [];
-                            var tmp_vars = exps[1].slice(1);
-                            var tmp_exps = exps.slice(2);
-                            for(var i = 0; i < tmp_vars.length; ++i) {
-                                variables.push(new SStr(tmp_vars[i]));
-                            }
-                            for(var i = 0; i < tmp_exps.length; ++i) {
-                                expressions.push(ast(tmp_exps[i]));
-                            }
-                            return new SDefine([
-                                new SStr(exps[0]), 
-                                new SStr(exps[1][0]), 
-                                new SLambda([
-                                    new SStr("lambda"), 
-                                    variables, 
-                                    new SBegin([
-                                        new SStr("begin"), 
-                                        expressions, 
-                                        
-                                    ]), 
-                                    
-                                ]), 
-                                
-                            ]);
-                        } else {
-                            return new SDefine([
-                                new SStr(exps[0]), 
-                                new SStr(exps[1]), 
-                                ast(exps[2]), 
-                                
-                            ]);
-                        }
-                    } else {
-                        if(exps[0] === 'set!') {
-                            return new SSet([
-                                new SStr(exps[0]), 
-                                ast(exps[1]), 
-                                ast(exps[2]), 
-                                
-                            ]);
-                        } else {
-                            if(exps[0] === 'lambda') {
-                                var variables = [];
-                                var expressions = [];
-                                var tmp = exps.slice(2);
-                                for(var i = 0; i < exps[1].length; ++i) {
-                                    variables.push(new SStr(exps[1][i]));
-                                }
-                                for(var i = 0; i < tmp.length; ++i) {
-                                    expressions.push(ast(tmp[i]));
-                                }
-                                return new SLambda([
-                                    new SStr(exps[0]), 
-                                    variables, 
-                                    new SBegin([
-                                        new SStr("begin"), 
-                                        expressions, 
-                                        
-                                    ]), 
-                                    
-                                ]);
-                            } else {
-                                if(exps[0] === 'begin') {
-                                    var begin = exps[0];
-                                    var tmp = exps.slice(1);
-                                    var expressions = [];
-                                    for(var i = 0; i < tmp.length; ++i) {
-                                        expressions.push(ast(tmp[i]));
-                                    }
-                                    return new SBegin([
-                                        new SStr(begin), 
-                                        expressions, 
-                                        
-                                    ]);
-                                } else {
-                                    if(exps[0] === 'let') {
-                                        var let = 'lambda';
-                                        var variables = [];
-                                        var values = [];
-                                        var expressions = [];
-                                        var tmp = exps.slice(2);
-                                        for(var i = 0; i < exps[1].length; ++i) {
-                                            variables.push(new SStr(exps[1][i][0]));
-                                            values.push(ast(exps[1][i][1]));
-                                        }
-                                        for(var i = 0; i < tmp.length; ++i) {
-                                            expressions.push(ast(tmp[i]));
-                                        }
-                                        var slambda = new SLambda([
-                                            new SStr(let), 
-                                            variables, 
-                                            new SBegin([
-                                                new SStr("begin"), 
-                                                expressions, 
-                                                
-                                            ]), 
-                                            
-                                        ]);
-                                        var proc = [
-                                            slambda
-                                        ];
-                                        for(var i = 0; i < values.length; ++i) {
-                                            proc.push(values[i]);
-                                        }
-                                        return new SProc(proc);
-                                    } else {
-                                        var expressions = [];
-                                        for(var i = 0; i < exps.length; ++i) {
-                                            var s = ast(exps[i]);
-                                            expressions.push(s);
-                                        }
-                                        return new SProc(expressions);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+    } else if(!(exps instanceof Array)) {
+        return new SNum(exps);
+    } else if(exps[0] === 'quote') {
+        return new SQuote([
+            new SStr(exps[0]), 
+            exps[1], 
+            
+        ]);
+    } else if(exps[0] === 'if') {
+        return new SIf([
+            new SStr(exps[0]), 
+            ast(exps[1]), 
+            ast(exps[2]), 
+            ast(exps[3]), 
+            
+        ]);
+    } else if(exps[0] === 'define') {
+        if(exps[1] instanceof Array) {
+            var variables = [];
+            var expressions = [];
+            var tmp_vars = exps[1].slice(1);
+            var tmp_exps = exps.slice(2);
+            for(var i = 0; i < tmp_vars.length; ++i) {
+                variables.push(new SStr(tmp_vars[i]));
             }
+            for(var i = 0; i < tmp_exps.length; ++i) {
+                expressions.push(ast(tmp_exps[i]));
+            }
+            return new SDefine([
+                new SStr(exps[0]), 
+                new SStr(exps[1][0]), 
+                new SLambda([
+                    new SStr("lambda"), 
+                    variables, 
+                    new SBegin([
+                        new SStr("begin"), 
+                        expressions, 
+                        
+                    ]), 
+                    
+                ]), 
+                
+            ]);
+        } else {
+            return new SDefine([
+                new SStr(exps[0]), 
+                new SStr(exps[1]), 
+                ast(exps[2]), 
+                
+            ]);
         }
+    } else if(exps[0] === 'set!') {
+        return new SSet([
+            new SStr(exps[0]), 
+            ast(exps[1]), 
+            ast(exps[2]), 
+            
+        ]);
+    } else if(exps[0] === 'lambda') {
+        var variables = [];
+        var expressions = [];
+        var tmp = exps.slice(2);
+        for(var i = 0; i < exps[1].length; ++i) {
+            variables.push(new SStr(exps[1][i]));
+        }
+        for(var i = 0; i < tmp.length; ++i) {
+            expressions.push(ast(tmp[i]));
+        }
+        return new SLambda([
+            new SStr(exps[0]), 
+            variables, 
+            new SBegin([
+                new SStr("begin"), 
+                expressions, 
+                
+            ]), 
+            
+        ]);
+    } else if(exps[0] === 'begin') {
+        var begin = exps[0];
+        var tmp = exps.slice(1);
+        var expressions = [];
+        for(var i = 0; i < tmp.length; ++i) {
+            expressions.push(ast(tmp[i]));
+        }
+        return new SBegin([
+            new SStr(begin), 
+            expressions, 
+            
+        ]);
+    } else if(exps[0] === 'let') {
+        var let = 'lambda';
+        var variables = [];
+        var values = [];
+        var expressions = [];
+        var tmp = exps.slice(2);
+        for(var i = 0; i < exps[1].length; ++i) {
+            variables.push(new SStr(exps[1][i][0]));
+            values.push(ast(exps[1][i][1]));
+        }
+        for(var i = 0; i < tmp.length; ++i) {
+            expressions.push(ast(tmp[i]));
+        }
+        var slambda = new SLambda([
+            new SStr(let), 
+            variables, 
+            new SBegin([
+                new SStr("begin"), 
+                expressions, 
+                
+            ]), 
+            
+        ]);
+        var proc = [
+            slambda
+        ];
+        for(var i = 0; i < values.length; ++i) {
+            proc.push(values[i]);
+        }
+        return new SProc(proc);
+    } else {
+        var expressions = [];
+        for(var i = 0; i < exps.length; ++i) {
+            var s = ast(exps[i]);
+            expressions.push(s);
+        }
+        return new SProc(expressions);
     }
 }
 function parse(str) {
@@ -459,20 +443,18 @@ function readFrom(tokens) {
             }
             tokens.shift();
             return L;
+        } else if(token === ")") {
+            console.log("SyntaxError: unexpected )");
         } else {
-            if(token === ")") {
-                console.log("SyntaxError: unexpected )");
-            } else {
-                var isNotANumber = isNaN;
-                if(!isNotANumber(token)) {
-                    if(token.indexOf(".") != -1) {
-                        return parseFloat(token);
-                    } else {
-                        return parseInt(token);
-                    }
+            var isNotANumber = isNaN;
+            if(!isNotANumber(token)) {
+                if(token.indexOf(".") != -1) {
+                    return parseFloat(token);
                 } else {
-                    return token;
+                    return parseInt(token);
                 }
+            } else {
+                return token;
             }
         }
     }
@@ -509,7 +491,7 @@ function makeFrame(origin, edge1, edge2) {
 function frameCoordMap(frame) {
     return function (v) {
         return addVect(frame.origin, addVect(scaleVect(v.x, frame.edge1), scaleVect(v.y, frame.edge2)));
-    }
+    };
 }
 function makeFullCanvasFrame(canvas) {
     return makeFrame(makeVect(0, 0), makeVect(canvas.clientWidth, 0), makeVect(0, canvas.clientHeight));
@@ -528,7 +510,7 @@ function segment2Painter(context, segments) {
         for(var i = 0; i < segments.length; ++i) {
             drawLine(context, frameCoordMap(frame)(segments[i].start), frameCoordMap(frame)(segments[i].end));
         }
-    }
+    };
 }
 function image2Painter(context, img) {
     return function (frame) {
@@ -537,14 +519,14 @@ function image2Painter(context, img) {
         context.setTransform(frame.edge1.x / w, frame.edge1.y / h, frame.edge2.x / w, frame.edge2.y / h, frame.origin.x, frame.origin.y);
         context.drawImage(img, 0, 0);
         context.setTransform(1, 0, 0, 1, 0, 0);
-    }
+    };
 }
 function transformPainter(painter, origin, corner1, corner2) {
     return function (frame) {
         var m = frameCoordMap(frame);
         var new_origin = m(origin);
         painter(makeFrame(new_origin, subVect(m(corner1), new_origin), subVect(m(corner2), new_origin)));
-    }
+    };
 }
 function flipVert(painter) {
     return transformPainter(painter, makeVect(0, 1), makeVect(1, 1), makeVect(0, 0));
@@ -566,14 +548,14 @@ function beside(painter1, painter2) {
         return function (frame) {
             paintLeft(frame);
             paintRight(frame);
-        }
+        };
     })(transformPainter(painter1, makeVect(0, 0), makeVect(0.5, 0), makeVect(0, 1)), transformPainter(painter2, makeVect(0.5, 0), makeVect(1, 0), makeVect(0.5, 1)));
 }
 function below(painter1, painter2) {
     return function (frame) {
         transformPainter(painter2, makeVect(0, 0), makeVect(1, 0), makeVect(0, 0.5))(frame);
         transformPainter(painter1, makeVect(0, 0.5), makeVect(1, 0.5), makeVect(0, 1))(frame);
-    }
+    };
 }
 function rightSplit(painter, n) {
     if(n == 0) {
